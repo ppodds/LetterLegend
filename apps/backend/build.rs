2313712d99a0f5731d1a3ab115a4fed5af8f0e09
ppodds <oscar20020629@gmail.com>
2023-04-30 13:39:37 +0800

    fix(backend): build script not working on linux

--- apps/backend/build.rs
+++ apps/backend/build.rs
@@ -1,9 +1,34 @@
 use std::io::Result;
 fn main() -> Result<()> {
-    prost_build::compile_protos(&["src/proto/player/*.proto"], &["src/proto/"])?;
-    prost_build::compile_protos(&["src/proto/control/*.proto"], &["src/proto/"])?;
-    prost_build::compile_protos(&["src/proto/lobby/*.proto"], &["src/proto/"])?;
-    prost_build::compile_protos(&["src/proto/game/*.proto"], &["src/proto/"])?;
-    prost_build::compile_protos(&["src/proto/error/*.proto"], &["src/proto/"])?;
+    prost_build::compile_protos(&["src/proto/player/player.proto"], &["src/proto/"])?;
+    prost_build::compile_protos(
+        &[
+            "src/proto/control/connect.proto",
+            "src/proto/control/disconnect.proto",
+            "src/proto/control/heartbeat.proto",
+        ],
+        &["src/proto/"],
+    )?;
+    prost_build::compile_protos(
+        &[
+            "src/proto/lobby/broadcast.proto",
+            "src/proto/lobby/create.proto",
+            "src/proto/lobby/join.proto",
+            "src/proto/lobby/list.proto",
+            "src/proto/lobby/lobby.proto",
+            "src/proto/lobby/quit.proto",
+            "src/proto/lobby/ready.proto",
+        ],
+        &["src/proto/"],
+    )?;
+    prost_build::compile_protos(
+        &[
+            "src/proto/game/board.proto",
+            "src/proto/game/start.proto",
+            "src/proto/game/tile.proto",
+        ],
+        &["src/proto/"],
+    )?;
+    prost_build::compile_protos(&["src/proto/error/error.proto"], &["src/proto/"])?;
     Ok(())
 }

0362932527b86bcee0843bd0064c41df700c4f9f
ppodds <oscar20020629@gmail.com>
2023-05-01 17:59:25 +0800

    fix: can't generate files with gen_proto script

--- gen_proto.ps1
+++ gen_proto.ps1
@@ -1,1 +1,13 @@
-protoc --csharp_out "apps/frontend/Assets/Scripts/Protos/Control" "apps/backend/src/proto/control/*.proto"
+$outBasePath = "apps/frontend/Assets/Scripts/Protos/"
+$protoBasePath = "apps/backend/src/proto/"
+
+$protoFolderMappingList = @(@("Control", "control/*.proto"), @("Error", "error/*.proto"), @("Game", "game/*.proto"), @("Lobby", "lobby/*.proto"), @("Player", "player/*.proto"))
+
+for ($i = 0; $i -lt $protoFolderMappingList.Length; $i++) {
+    $outPath = $outBasePath + $protoFolderMappingList[$i][0]
+    if (!(Test-Path -Path $outPath)) {
+        New-Item -ItemType Directory -Path $outPath
+    }
+    $protoPath = $protoBasePath + $protoFolderMappingList[$i][1]
+    protoc --csharp_out $outPath -I $protoBasePath $protoPath
+}

b6bd875562d0414381e1174b93a6e55168eef078
ppodds <oscar20020629@gmail.com>
2023-05-24 15:58:12 +0800

    refactor: remove comment-out code

--- apps/backend/src/connection.rs
+++ apps/backend/src/connection.rs
@@ -30,37 +30,6 @@ impl Connection {
         }
     }
 
-    // pub fn try_read_frame(
-    //     &mut self,
-    // ) -> Result<Option<Frame>, Box<dyn std::error::Error + Send + Sync>> {
-    //     // Attempt to parse a frame from the buffered data. If
-    //     // enough data has been buffered, the frame is
-    //     // returned.
-    //     if let Some(frame) = self.parse_frame()? {
-    //         return Ok(Some(frame));
-    //     }
-
-    //     // There is not enough buffered data to read a frame.
-    //     // Attempt to read more data from the socket.
-    //     //
-    //     // On success, the number of bytes is returned. `0`
-    //     // indicates "end of stream".
-    //     match self.reader.try_read_buf(&mut self.buffer) {
-    //         Ok(0) => {
-    //             // The remote closed the connection. For this to be
-    //             // a clean shutdown, there should be no data in the
-    //             // read buffer. If there is, this means that the
-    //             // peer closed the socket while sending a frame.
-    //             if self.buffer.is_empty() {
-    //                 return Err("connection safe closed".into());
-    //             } else {
-    //                 return Err("connection reset by peer".into());
-    //             }
-    //         }
-    //         Ok(_) | Err(_) => Ok(None),
-    //     }
-    // }
-
     pub async fn read_frame(
         &self,
     ) -> Result<Option<Frame>, Box<dyn std::error::Error + Send + Sync>> {

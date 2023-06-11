64bfe178a192edec3a0f6ad3e777a5ed72fc6ee3
ppodds <oscar20020629@gmail.com>
2023-06-11 20:35:43 +0800

    fix: resource not clean

--- apps/backend/src/main.rs
+++ apps/backend/src/main.rs
@@ -4,8 +4,11 @@ use backend::server::Server;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn Error + Send + Sync>> {
+    println!("Loading environment variables");
     dotenvy::dotenv().unwrap();
+    println!("Building server");
     let server = Server::new().await?;
+    println!("Start server");
     server.run().await?;
     Ok(())
 }
